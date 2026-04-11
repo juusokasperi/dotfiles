fdc41f497e2378f1a2a76d0433a8ba53a16f57f8
juusokasperi <juusor@gmail.com>
2026-04-11 20:18:42 +0300

    reordered echo after declaring the variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Linking ~/.tmux.conf -> $DOTFILES_DIR/.tmux.conf"
-
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo "Linking ~/.tmux.conf -> $DOTFILES_DIR/.tmux.conf"
 ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
+
 echo "Bye."
